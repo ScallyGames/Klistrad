7d5d8570215ec15eb4be800f69fb005aefc9e8de
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2021-04-23 18:25:41 +0200

    Project boilerplate

---
+++ webpack.dev.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/scripts/index.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-loader',
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            name: 'style.css',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'stylus-loader',
+                    },
+                ]
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.pug',
+            minify: false,
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+    },
+};
