869295cca7f26a222cbfe8dc7a7e47ac9a604c78
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2021-04-24 18:33:36 +0200

    Add minor map adjustments

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -22,29 +22,29 @@ class Game
 
         this.addObject(new DebugPosition()); //delete me before release
 
-        let gateTopIn = new Gate(new Vector2(89, 4), new Vector2(-2, -1), 'i', GateDirection.Left);
+        let gateTopIn = new Gate(new Vector2(88, 4), new Vector2(-2, -1), 'i', GateDirection.Left);
         this.addObject(gateTopIn);
-        let gateTopOut = new Gate(new Vector2(92, 4), new Vector2(3, -1), 'o', GateDirection.Right);
+        let gateTopOut = new Gate(new Vector2(91, 4), new Vector2(3, -1), 'o', GateDirection.Right);
         this.addObject(gateTopOut);
-        let gateBottomOut = new Gate(new Vector2(73, 38), new Vector2(-2, 1), 'k', GateDirection.Left);
+        let gateBottomOut = new Gate(new Vector2(72, 38), new Vector2(-2, 1), 'k', GateDirection.Left);
         this.addObject(gateBottomOut);
-        let gateBottomIn = new Gate(new Vector2(76, 38), new Vector2(3, 1), 'l', GateDirection.Right);
+        let gateBottomIn = new Gate(new Vector2(75, 38), new Vector2(3, 1), 'l', GateDirection.Right);
         this.addObject(gateBottomIn);
 
-        this.addObject(new GatePost(new Vector2(91, 4)));
-        this.addObject(new GatePost(new Vector2(75, 38)));
+        this.addObject(new GatePost(new Vector2(90, 4)));
+        this.addObject(new GatePost(new Vector2(74, 38)));
         
         this.addObject(new Vat(new Vector2(30, 15)));
 
         let waterTank = new WaterTank(new Vector2(73, 7), 'j');
         this.addObject(waterTank);
         this.waypointsCarOne = [
-            new Vector2(90, -6),
-            new Vector2(90, 2),
+            new Vector2(89, -6),
+            new Vector2(89, 2),
             ()=>{
                 return gateTopIn.isOpen;
             },
-            new Vector2(90, 8),
+            new Vector2(89, 8),
             new Vector2(114, 8),
             new Vector2(114, 15),
             new Vector2(100, 15),
@@ -54,36 +54,36 @@ class Game
             new Vector2(100, 16),
             new Vector2(114, 16),
             new Vector2(114, 35),
-            new Vector2(74, 35),
-            new Vector2(74, 36),
+            new Vector2(73, 35),
+            new Vector2(73, 36),
             ()=>{
                 return gateBottomOut.isOpen;
             },
             new Vector2(74, 44)
         ];
         this.waypointsCarTwo = [
-            new Vector2(76, 45),
-            new Vector2(76, 39),
+            new Vector2(75, 45),
+            new Vector2(75, 39),
             ()=>{
                 return gateBottomIn.isOpen;
             },
-            new Vector2(76, 36),
+            new Vector2(75, 36),
             new Vector2(116, 36),
-            new Vector2(116, 32),
-            new Vector2(100, 32),
+            new Vector2(116, 31),
+            new Vector2(100, 31),
             ()=>{
                 console.log("waiting for unloading")
                 return true;
             },
-            new Vector2(100, 33),
-            new Vector2(116, 33),
+            new Vector2(100, 32),
+            new Vector2(116, 32),
             new Vector2(116, 7),
-            new Vector2(92, 7),
-            new Vector2(92, 5),
+            new Vector2(91, 7),
+            new Vector2(91, 5),
             ()=>{
                 return gateTopOut.isOpen;
             },
-            new Vector2(92, -6)
+            new Vector2(91, -6)
         ];
         this.addObject(new Car([...this.waypointsCarOne], 100));
         this.addObject(new Car([...this.waypointsCarTwo], 100));
